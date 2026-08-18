e8f6127dd73efa3a0f9d4b7b190f5eaea02f8d4c
cmdrecords <f.50tunit@mail.ru>
2026-08-18 17:03:27 +0300

    add slider services

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
-console.log("test");
+let btnNext = document.querySelector(".arrow-next");
+
+let sliderCards = document.querySelectorAll(".slide");
+
+btnNext.onclick = () => {
+  sliderCards.forEach(
+    (element) => (element.style.transform = `${element.style.transform} - 40%`)
+  );
+};
